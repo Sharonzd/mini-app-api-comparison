989bde4e540803e30241cd3033fd2365a4686064
zhoudan03 <zhoudan03@baidu.com>
2019-09-17 01:36:42 +0800

    fix publish

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ vuepress build
 mv output output_back
 git checkout gh-pages
 mv output_back/* .
-mv output_back/assets .
+mv output_back/assets/ .
 rm -rf output_back
 git add .
 git commit -m 'publish'
