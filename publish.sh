dfbae1bab5525bfcefc82b5f621f955ff849efb5
zhoudan03 <zhoudan03@baidu.com>
2019-09-17 01:30:10 +0800

    修正脚本

--- publish.sh
+++ publish.sh
@@ -3,7 +3,8 @@
 vuepress build
 mv output output_back
 git checkout gh-pages
-mv output_back ..
+mv output_back/* .
+rm -rf outout_back
 git add .
 git commit -m 'publish'
 git push --set-upstream origin gh-pages
